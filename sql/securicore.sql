ce967b702766f2c94d43daad8a66356c9b6d40f6
Barbara.Cam <cam.barbara@hotmail.com>
2022-04-20 01:07:40 -0400

    add sql file

--- sql/securicore.sql
+++ sql/securicore.sql
@@ -1,6 +1,12 @@
 -- SQL code for securicore database table
 -- Database Name: securicore
 
+CREATE TABLE `users` (
+  `user_id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
 CREATE TABLE `userInfo` (
   `userInfo_id` int PRIMARY KEY AUTO_INCREMENT,
   `uname` varchar(255) NOT NULL,  
@@ -9,11 +15,7 @@ CREATE TABLE `userInfo` (
 );
 ALTER TABLE `userInfo` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
-CREATE TABLE `users` (
-  `user_id` int PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-);
+
 
 -- Insert data with a hashed password
 INSERT INTO users(username, password) values ("pepe", MD5("1234"));
